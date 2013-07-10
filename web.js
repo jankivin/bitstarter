6e688f2622b4f8098ebcdb59fd6c40b2e9622b39
Janne Kivinen <jhkivinen@gmail.com>
2013-07-11 00:58:36 +0300

    edited web.js

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-// var file = "index.html":
+//// var file = "index.html":
 
-// var textstring = fs.readFileSync(file);
-// console.log(
+//// var textstring = fs.readFileSync(file);
+//// console.log(
 
 var data;
 data = fs.readFileSync('index.html','utf8');
 
+//console.log(data);
+
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
+////  response.send('Hello World 2!');
     response.send(data);
 });
 
