a459eae7d7f3f2f17e7122b38e10d9d886b098c2
Janne Kivinen <jhkivinen@gmail.com>
2013-07-15 08:19:22 +0300

    HW4 part 1 changes

--- grader.js
+++ grader.js
@@ -27,6 +27,9 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+// JaKi addition
+// var CHECKSURL_DEFAULT="http://hidden-river-4976.herokuapp.com/";
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -65,6 +68,9 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+// JaKi add starts
+//        .option('u, --url <url>', 'URL to index.html', 1, CHECKSURL_DEFAULT) 
+// JaKi add ends
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
